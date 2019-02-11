2d2e224e5d2d15edb8f24d89c9bb6177cadb9223
Piotr Rasztemborski <raszta9999@wp.pl>
2019-02-11 23:26:23 +0100

    Add footer and menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { appRoutes } from './routes';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    MenuComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
