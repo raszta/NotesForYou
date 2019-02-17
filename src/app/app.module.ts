2f484bffee911c8fafe01086c0d8f124f41b3d9a
Piotr Rasztemborski <raszta9999@wp.pl>
2019-02-17 18:41:35 +0100

    Add user-settings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { RegisterComponent } from './register/register.component';
     DashboardComponent,
     LoginComponent,
     RegisterComponent,
+    UserSettingsComponent,
   ],
   imports: [
     BrowserModule,
