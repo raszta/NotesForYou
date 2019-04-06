8103b69beae047100ca3bdd5b62eb530bd0652fd
Piotr Rasztemborski <raszta9999@wp.pl>
2019-04-06 12:57:43 +0200

    Add authrepository implementation

--- NotesForYou.API/Data/DataContext.cs
+++ NotesForYou.API/Data/DataContext.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using NotesForYou.API.Models;
 
 namespace NotesForYou.API.Data {
     public class DataContext : DbContext {
         public DataContext (DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<User> Users { get; set; }
     }
 }
