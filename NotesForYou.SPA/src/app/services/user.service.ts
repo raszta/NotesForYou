1d4c481018b62eed104efff4a85183fd6d013c41
Piotr Rasztemborski <raszta9999@wp.pl>
2019-04-11 19:30:52 +0200

    Add login to auth service

--- NotesForYou.SPA/src/app/services/user.service.ts
+++ NotesForYou.SPA/src/app/services/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+  decodedToken: any;
+  jwtHelper = new JwtHelperService();
 
-  constructor() { }
+  constructor(
+    private htppClient: HttpClient
+  ) { }
+
+  login(model: any) {
+    this.htppClient.post(this.baseUrl, model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        }
+      })
+    );
+  }
 }
--- NotesForYou.SPA/src/app/user/user.module.ts
+++ NotesForYou.SPA/src/app/user/user.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserSettingsComponent } from './user-settings/user-settings.component';
-import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,6 @@ import { AuthService } from './auth.service';
     CommonModule
   ],
   providers: [
-    AuthService
   ],
 })
 export class UserModule { }
