9dde667c3335d07efe79384b47f1d39bc026670b
Piotr Rasztemborski <raszta9999@wp.pl>
2019-04-11 19:42:01 +0200

    Add error handling to server side

--- NotesForYou.API/Startup.cs
+++ NotesForYou.API/Startup.cs
@@ -47,6 +47,17 @@ namespace NotesForYou {
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
             } else {
+                app.UseExceptionHandler (builder => {
+                    builder.Run (async context => {
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature> ();
+                        if (error != null) {
+                            context.Response.AddapplicationError (error.Error.Message);
+                            await context.Response.WriteAsync (error.Error.Message);
+                        }
+                    });
+                });
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 // app.UseHsts();
             }
