0d431031fe04c09218b077995c544f2f790fa82e
Piotr Rasztemborski <raszta9999@wp.pl>
2019-12-15 18:33:46 +0100

    Changes in gitignore

--- NotesForYou.API/Startup.cs
+++ NotesForYou.API/Startup.cs
@@ -99,11 +99,10 @@ namespace NotesForYou {
                         }
                     });
                 });
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 // app.UseHsts();
             }
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseCors (x => x.AllowAnyHeader ().AllowAnyMethod ().AllowAnyOrigin ());
             app.UseAuthentication ();
             app.UseMvc ();
